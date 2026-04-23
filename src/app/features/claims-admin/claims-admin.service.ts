578262fcfe98ba3936d1b56e897c4f0b18ca6494
yassmine44 <yassmine.megbli@esprit.tn>
2026-04-23 15:48:49 +0100

    UPDATE CLAIM

--- src/app/features/claims-admin/claims-admin.service.ts
+++ src/app/features/claims-admin/claims-admin.service.ts
@@ -5,11 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ClaimsAdminService {
 
-<<<<<<< HEAD
   private api = 'http://localhost:8089/api/insurance/claims';
-=======
-  private api = 'http://localhost:8082/api/insurance/claims';
->>>>>>> eya
 
   private isBrowser(): boolean {
     return typeof window !== 'undefined';
@@ -36,7 +32,6 @@ export class ClaimsAdminService {
     if (!this.isBrowser()) return Promise.resolve([]);
 
     return this.getAll().then((claims: any[]) => {
-      // Filtrage local car le endpoint by-client pour les claims peut ne pas exister
       return claims.filter(c => c.voucher?.client?.id === clientId);
     });
   }
@@ -70,11 +65,7 @@ export class ClaimsAdminService {
 
   // ===== PRODUCTS (LISTE → length) =====
   getProducts(): Promise<any[]> {
-<<<<<<< HEAD
     return fetch(`http://localhost:8089/api/partner-products/all`, {
-=======
-    return fetch(`http://localhost:8082/api/partner-products/all`, {
->>>>>>> eya
       headers: {
         Authorization: `Bearer ${this.getToken()}`
       }
@@ -83,11 +74,7 @@ export class ClaimsAdminService {
 
   // ===== INSURANCES (LISTE → length) =====
   getInsurances(): Promise<any[]> {
-<<<<<<< HEAD
     return fetch(`http://localhost:8089/api/insurance/companies/all`, {
-=======
-    return fetch(`http://localhost:8082/api/insurance/companies/all`, {
->>>>>>> eya
       headers: {
         Authorization: `Bearer ${this.getToken()}`
       }
